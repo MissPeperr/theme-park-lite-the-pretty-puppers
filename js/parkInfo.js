bde10bbd08546574bc47ca4efc4a52d75f294579
MissPeperr <madisonpeper@gmail.com>
2018-08-02 08:50:44 -0500

    idk what i did lol

--- js/parkInfo.js
+++ js/parkInfo.js
@@ -9,10 +9,8 @@ db.fetchParkInfo()
 .then((result) => {
     // and setting it inside of parkInfo
    parkInfo = result;
-   console.log("fetchParkInfo:", parkInfo[0].description);
    // use function from parkInfoDOM
     parkInfoDOM(parkInfo);
 });
 
-// use a forEach loop to get access to each item inside of each attraction
 
