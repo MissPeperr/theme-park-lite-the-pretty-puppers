1163a403fef4b2a246edb2a9fe0831f570db4ce5
MissPeperr <madisonpeper@gmail.com>
2018-08-02 11:23:40 -0500

    added css into attractions.css from the js

--- js/attractionHTML.js
+++ js/attractionHTML.js
@@ -3,11 +3,11 @@
 
 
 function newAttractionCard (name, description) {
-        return `<div style= 'background-color: rgb(31, 114, 50, .100); border-radius: 25px; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);'>
-        <h2 style='color: beige; text-align: left; margin: 1em 0 .25em 0; padding: 5px 0 0 15px; text-shadow: 2px 2px 8px #000000; font-size: 2.5em; font-family: "Mouse Memoirs", sans serif'>
+        return `<div id='attraction-container'>
+        <h2 id='attraction-h2'>
         ${name} 
         </h2>
-        <p style='font-family: "Questrial", sans serif; margin-top: 0; padding: 10px; color: black; font-size: 18px'>
+        <p id='attraction-description'>
         ${description}
         </p>`
 
